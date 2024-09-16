f2ea87d37b952a2a738ad8291bd85b5bb00d4249
buihien0109 <buihien01091997@gmail.com>
2024-09-16 20:50:19 +0700

    update

---
+++ springboot/day_18/movie-app/src/main/java/com/example/movieapp/service/ReviewService.java
@@ -0,0 +1,78 @@
+package com.example.movieapp.service;
+
+import com.example.movieapp.entity.Movie;
+import com.example.movieapp.entity.Review;
+import com.example.movieapp.entity.User;
+import com.example.movieapp.model.request.CreateReviewRequest;
+import com.example.movieapp.model.request.UpdateReviewRequest;
+import com.example.movieapp.repository.MovieRepository;
+import com.example.movieapp.repository.ReviewRepository;
+import com.example.movieapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+    private final MovieRepository movieRepository;
+    private final UserRepository userRepository;
+
+    public Review createReview(CreateReviewRequest request) {
+        // TODO: Fix user. Sau này user sẽ là user đang đăng nhập
+        Integer userId = 1;
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + userId));
+
+        Movie movie = movieRepository.findById(request.getMovieId())
+                .orElseThrow(() -> new RuntimeException("Movie not found with id: " + request.getMovieId()));
+
+        // TODO: Bổ sung validate rating, content
+        Review review = Review.builder()
+                .content(request.getContent())
+                .rating(request.getRating())
+                .movie(movie)
+                .user(user)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+        return reviewRepository.save(review);
+    }
+
+    public Review updateReview(Integer id, UpdateReviewRequest request) {
+        // TODO: Fix user. Sau này user sẽ là user đang đăng nhập
+        Integer userId = 1;
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + userId));
+
+        Review review = reviewRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Review not found with id: " + id));
+
+        if (!review.getUser().getId().equals(user.getId())) {
+            throw new RuntimeException("You are not owner of this review");
+        }
+
+        // TODO: Bổ sung validate rating, content
+        review.setContent(request.getContent());
+        review.setRating(request.getRating());
+        review.setUpdatedAt(LocalDateTime.now());
+        return reviewRepository.save(review);
+    }
+
+    public void deleteReview(Integer id) {
+        // TODO: Fix user. Sau này user sẽ là user đang đăng nhập
+        Integer userId = 1;
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + userId));
+
+        Review review = reviewRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Review not found with id: " + id));
+
+        if (!review.getUser().getId().equals(user.getId())) {
+            throw new RuntimeException("You are not owner of this review");
+        }
+        reviewRepository.delete(review);
+    }
+}
