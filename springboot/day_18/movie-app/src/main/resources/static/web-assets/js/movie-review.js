c6b18d25aa9c4a5fabcab20aa756d1bd15ec662c
buihien0109 <buihien01091997@gmail.com>
2024-09-20 20:03:21 +0700

    update

--- springboot/day_18/movie-app/src/main/resources/static/web-assets/js/movie-review.js
+++ springboot/day_18/movie-app/src/main/resources/static/web-assets/js/movie-review.js
@@ -37,6 +37,41 @@ function highlightStars(rating) {
     });
 }
 
+// Hien thi danh sach review
+const reviewListEl = document.querySelector(".review-list");
+const renderReviews = reviews => {
+    let html = "";
+    reviews.forEach(review => {
+        html += `
+            <div class="rating-item d-flex align-items-center mb-3 pb-3">
+                <div class="rating-avatar">
+                    <img src="${review.user.avatar}" alt="${review.user.name}">
+                </div>
+                <div class="rating-info ms-3">
+                    <div class="d-flex align-items-center">
+                        <p class="rating-name mb-0"><strong>${review.user.name}</strong></p>
+                        <p class="rating-time mb-0 ms-2">${formatDate(review.createdAt)}</p></div>
+                    <div class="rating-star">
+                        <p class="mb-0 fw-medium">
+                            <span class="rating-icon me-1"><i class="fa fa-star"></i></span>
+                            <span>${review.rating}/10</span>
+                        </p>
+                    </div>
+                    <p class="rating-content mt-1 mb-0 text-muted">${review.content}</p></div>
+            </div>
+        `
+    });
+    reviewListEl.innerHTML = html;
+}
+
+const formatDate = dateString => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = ("0" + (date.getMonth() + 1)).slice(-2); // 09 -> 09 , 011 -> 11
+    const day = ("0" + date.getDate()).slice(-2);
+    return `${day}/${month}/${year}`;
+}
+
 const formReviewEl = document.getElementById("form-review");
 formReviewEl.addEventListener("submit", () => {
 
