a4c3ccf27313826e4a42338a6ddba9a0b4ea3cba
buihien0109 <buihien01091997@gmail.com>
2024-11-01 20:04:50 +0700

    update

---
+++ springboot/day_37/demo-spring-security-thymeleaf/src/main/java/com/example/demospringsecurity/security/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.example.demospringsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true
+)
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService users() {
+        // The builder will ensure the passwords are encoded before saving in memory
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails user = users
+                .username("user")
+                .password("123")
+                .roles("USER")
+                .build();
+        UserDetails admin = users
+                .username("admin")
+                .password("123")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        String[] PUBLIC_PATH = {"/aa", "/bb", "/css/**", "/js/**", "/images/**"};
+        http.authorizeHttpRequests(authorizeRequests -> {
+//            authorizeRequests.requestMatchers("/", "/a", "/b").permitAll();
+//            authorizeRequests.requestMatchers(PUBLIC_PATH).permitAll();
+//            authorizeRequests.requestMatchers("/user").hasRole("USER");
+//            authorizeRequests.requestMatchers("/admin").hasRole("ADMIN");
+//            authorizeRequests.requestMatchers("/blogs", "/admin/users").hasAnyRole("USER", "ADMIN");
+//            authorizeRequests.requestMatchers(HttpMethod.GET, "/movies", "/episodes").hasAnyRole("USER", "ADMIN");
+//            authorizeRequests.requestMatchers("/actors").hasAuthority("ROLE_ADMIN");
+//            authorizeRequests.anyRequest().authenticated();
+
+            authorizeRequests.anyRequest().permitAll();
+        });
+
+        http.formLogin(formLogin -> {
+            formLogin.defaultSuccessUrl("/", true);
+            formLogin.loginPage("/login");
+            formLogin.loginProcessingUrl("/login-process");
+            formLogin.usernameParameter("email");
+            formLogin.passwordParameter("credential");
+        });
+
+        http.logout(logout -> {
+            logout.logoutSuccessUrl("/");
+        });
+        return http.build();
+    }
+}
